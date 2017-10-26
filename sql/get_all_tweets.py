d8de72f347b02ae460f5df3f1af1c81b65f0f37d
Mauricio <waxkun@gmail.com>
2017-10-26 15:43:24 -0300

    sql module

--- sql/get_all_tweets.py
+++ sql/get_all_tweets.py
@@ -16,6 +16,7 @@ logging.basicConfig(format='%(asctime)s | %(name)s | %(levelname)s : %(message)s
 # m3
 server, engine = connect_to_server(username='mquezada',
                                    host="m3.dcc.uchile.cl",
+                                   ssh_pkey="/home/2015/mquezada/.ssh/id_rsa",
                                    db_user="mquezada",
                                    db_name="mquezada_db",
                                    db_password="phoophoh7ahdaiJahphoh3aicooz7uka3ahJe9oi")
