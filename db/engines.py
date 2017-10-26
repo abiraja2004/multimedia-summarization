e81729af4cf569b6e4796339013cbb019c8660e4
Mauricio <waxkun@gmail.com>
2017-10-26 15:51:51 -0300

    sql module

--- db/engines.py
+++ db/engines.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sshtunnel import SSHTunnelForwarder
+from contextlib import contextmanager
 
 engine = create_engine('mysql://bnews_user:ur5Eesiedie3ja6ahkiX@localhost/breakingnews', encoding='utf-8')
 engine_mquezada = create_engine('mysql://root@localhost/ams?charset=utf8mb4', encoding='utf-8')
@@ -9,6 +10,7 @@ engine_m3 = create_engine('mysql://mquezada:phoophoh7ahdaiJahphoh3aicooz7uka3ahJ
 engine_of215 = create_engine('mysql://root@localhost/twitter_news?charset=utf8mb4', encoding='utf-8')
 
 
+@contextmanager
 def connect_to_server(username, host, db_user="root", db_name="twitter_news", db_password="",
                       ssh_pkey="/home/mquezada/.ssh/id_rsa"):
     server = SSHTunnelForwarder(
@@ -18,8 +20,10 @@ def connect_to_server(username, host, db_user="root", db_name="twitter_news", db
         remote_bind_address=('127.0.0.1', 3306))
     server.start()
 
-    engine = create_engine(f'mysql://{db_user}:{db_password}@127.0.0.1:{server.local_bind_port}'
+    engine_ = create_engine(f'mysql://{db_user}:{db_password}@127.0.0.1:{server.local_bind_port}'
                            f'/{db_name}?charset=utf8mb4',
                            encoding='utf-8')
 
-    return server, engine
+    yield engine_
+
+    server.stop()
--- sql/get_all_tweets.py
+++ sql/get_all_tweets.py
@@ -13,17 +13,19 @@ from document_generation.documents import get_representatives
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s | %(name)s | %(levelname)s : %(message)s', level=logging.INFO)
 
-# m3
-server, engine = connect_to_server(username='mquezada',
-                                   host="m3.dcc.uchile.cl",
-                                   ssh_pkey="/home/2015/mquezada/.ssh/id_rsa",
-                                   db_user="mquezada",
-                                   db_name="mquezada_db",
-                                   db_password="phoophoh7ahdaiJahphoh3aicooz7uka3ahJe9oi")
+m3 = lambda: connect_to_server(username='mquezada',
+                               host="m3.dcc.uchile.cl",
+                               ssh_pkey="/home/2015/mquezada/.ssh/id_rsa",
+                               db_user="mquezada",
+                               db_name="mquezada_db",
+                               db_password="phoophoh7ahdaiJahphoh3aicooz7uka3ahJe9oi")
 
-Session = sessionmaker(engine, autocommit=True)
-session = Session()
+with m3() as engine:
+    Session = sessionmaker(engine, autocommit=True)
+    session = Session()
 
-for component_id in range(1, 25481 + 1):
-    events = session.query()
-all_tweets = session.query(Tweet).yield_per(100000).filter(Tweet.event_id_id ==)
+    for component_id in range(1, 25481 + 1):
+        events = session.query(ComponentEvent).filter(ComponentEvent.component_id == component_id).all()
+
+    #all_tweets = session.query(Tweet).yield_per(100000).filter(Tweet.event_id_id ==)
+
