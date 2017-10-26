678c10ea0b6f910d91583495da2509c05f71f51f
Mauricio <waxkun@gmail.com>
2017-10-26 15:43:52 -0300

    sql module

--- db/engines.py
+++ db/engines.py
@@ -9,10 +9,11 @@ engine_m3 = create_engine('mysql://mquezada:phoophoh7ahdaiJahphoh3aicooz7uka3ahJ
 engine_of215 = create_engine('mysql://root@localhost/twitter_news?charset=utf8mb4', encoding='utf-8')
 
 
-def connect_to_server(username, host, db_user="root", db_name="twitter_news", db_password=""):
+def connect_to_server(username, host, db_user="root", db_name="twitter_news", db_password="",
+                      ssh_pkey="/home/mquezada/.ssh/id_rsa"):
     server = SSHTunnelForwarder(
         (host, 22),
-        ssh_pkey="/home/mquezada/.ssh/id_rsa",
+        ssh_pkey=ssh_pkey,
         ssh_username=username,
         remote_bind_address=('127.0.0.1', 3306))
     server.start()
